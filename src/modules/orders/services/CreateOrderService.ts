dd6fbfdbba0d3e6b795e6c5c7fbf500d6386b3d0
Luciano Tavernard <lucianotavernard@gmail.com>
2020-05-13 22:09:53 -0300

    chore: adicionado o código do desafio de relacionamento com o banco de dados

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -18,16 +18,64 @@ interface IRequest {
 }
 
 @injectable()
-class CreateProductService {
+class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('User not found');
+    }
+
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (!findProducts) {
+      throw new AppError('Product not found');
+    }
+
+    const findProductsInStock = products.every(product => {
+      const findedProduct = findProducts.find(
+        storedProduct => storedProduct.id === product.id,
+      );
+
+      return product.quantity < (findedProduct?.quantity || 0);
+    });
+
+    if (!findProductsInStock) {
+      throw new AppError('Insufficient product stock');
+    }
+
+    const orderProducts = products.map(product => {
+      const findedProduct = findProducts.find(
+        storedProduct => storedProduct.id === product.id,
+      );
+
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: findedProduct?.price || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
-export default CreateProductService;
+export default CreateOrderService;
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,64 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const findProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return findProducts ?? [];
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const findProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    const updatedProductsPromises = findProducts.map(storedProduct => {
+      const findedProduct = products.find(
+        product => product.id === storedProduct.id,
+      );
+
+      const quantity = findedProduct?.quantity || 0;
+
+      return this.ormRepository.save({
+        ...storedProduct,
+        quantity: storedProduct.quantity - quantity,
+      });
+    });
+
+    return Promise.all(updatedProductsPromises);
   }
 }
 
